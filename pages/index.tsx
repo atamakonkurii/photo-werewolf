82127cc5dc3b11a4fdeed39ac92376def7ed0c77
atamakonkurii <morisudesu@gmail.com>
2022-04-12 06:14:56 +0900

    ♻️ templatesフォルダに移動

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { CustomNextPage } from "next";
 
 import { PreGameLayout } from "@/component/layout";
-import { Index } from "@/component/pages/index/index";
+import { Index } from "@/component/templates/index/index";
 
 const IndexPage: CustomNextPage = () => {
   return <Index />;
