65bc27124549f88e3558d1517b228909fa588ad0
atamakonkurii <morisudesu@gmail.com>
2022-04-12 21:36:16 +0900

    💄 formを追加

--- src/component/templates/room/new.tsx
+++ src/component/templates/room/new.tsx
@@ -1,5 +1,47 @@
+/* eslint-disable no-console */
+import { Box, Button, Group, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import type { VFC } from "react";
 
 export const RoomNew: VFC = () => {
-  return <h2 className="text-gray-300">CreateRoom!</h2>;
+  const form = useForm({
+    initialValues: {
+      roomName: "",
+      // termsOfService: false,
+    },
+
+    validate: {
+      // eslint-disable-next-line arrow-body-style
+      roomName: (value) => {
+        return /^\S+@\S+$/.test(value) ? null : "Invalid roomName";
+      },
+    },
+  });
+
+  return (
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form
+        onSubmit={form.onSubmit((values) => {
+          return console.log(values);
+        })}
+      >
+        <TextInput
+          required
+          label="部屋名"
+          placeholder="てつこさんの部屋"
+          {...form.getInputProps("roomName")}
+        />
+
+        {/* <Checkbox
+          mt="md"
+          label="I agree to sell my privacy"
+          {...form.getInputProps("termsOfService", { type: "checkbox" })}
+        /> */}
+
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
+  );
 };
