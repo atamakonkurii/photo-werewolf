066b4728004f48717cef6f8ad775fbb4aee1527a
atamakonkurii <morisudesu@gmail.com>
2022-04-14 08:44:34 +0900

    buildがとうらないので修正

--- src/component/templates/room/roomId/standby/index.tsx
+++ src/component/templates/room/roomId/standby/index.tsx
@@ -1,7 +1,7 @@
 import type { VFC } from "react";
 
 import { Title } from "@/component/atoms/Title";
-import { CopyLinkButton } from "@/component/molecules/Button/CopyLinkButton";
+// import { CopyLinkButton } from "@/component/molecules/Button/CopyLinkButton";
 import { LinkButton } from "@/component/molecules/Button/LinkButton";
 import { Guests } from "@/component/organisms/Guests";
 
@@ -9,7 +9,8 @@ export const RoomIdStandBy: VFC = () => {
   return (
     <div className="flex flex-col justify-center items-center p-16">
       <Title title="待機部屋" />
-      <CopyLinkButton url={`${document.URL}/guests/new`} />
+      TODO:buildできるように変更
+      {/* <CopyLinkButton url={`${document.URL}/guests/new`} /> */}
       <div className="mt-8" />
       <Guests />
       <div className="mt-8" />
