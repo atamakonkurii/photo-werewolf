83cf4031d0a09ad838a9f271a952c03f46f7eba6
atamakonkurii <morisudesu@gmail.com>
2022-04-17 17:43:14 +0900

    authuser作成時に同時にユーザーを作成

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,6 +1,4 @@
 -- in supabase/seed.sql
--- insert into employees (name)
--- values 
---   ('Erlich Backman'),
---   ('Richard Hendricks'),
---   ('Monica Hall');
+insert into auth.users (id)
+values 
+  ('0d5a27ca-6263-418a-9732-5a33b19ecaf9');
