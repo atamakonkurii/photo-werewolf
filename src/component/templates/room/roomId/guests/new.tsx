7ee88dca35d36d085b8ccb0dce70fab48a78e0c1
atamakonkurii <morisudesu@gmail.com>
2022-04-19 23:34:30 +0900

    game_resultsレコードを作成

--- src/component/templates/room/roomId/guests/new.tsx
+++ src/component/templates/room/roomId/guests/new.tsx
@@ -19,6 +19,7 @@ const schema = z.object({
 export const GuestsNew: VFC = () => {
   const router = useRouter();
   const standByPath = router.asPath.replace("/guests/new", "");
+  const roomId = standByPath.split("/")[2];
 
   const form = useForm({
     schema: zodResolver(schema),
@@ -29,6 +30,7 @@ export const GuestsNew: VFC = () => {
 
   const handleSubmit = async (values: { name: string }) => {
     const guestId = nanoid();
+    // TODO:dbfunctionでひとまとめにする
     await supabase
       .from("users")
       // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -38,6 +40,11 @@ export const GuestsNew: VFC = () => {
       .from("guest_users")
       // eslint-disable-next-line @typescript-eslint/naming-convention
       .insert([{ user_id: guestId, name: values.name }]);
+
+    await supabase
+      .from("game_results")
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      .insert([{ user_id: guestId, room_id: roomId }]);
 
     router.push({
       pathname: standByPath,
