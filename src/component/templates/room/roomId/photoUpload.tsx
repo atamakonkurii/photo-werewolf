ead205b1186cafbed1db073dab60951f88cef172
atamakonkurii <morisudesu@gmail.com>
2022-04-24 16:04:15 +0900

    画像をアップロードできるようにした

--- src/component/templates/room/roomId/photoUpload.tsx
+++ src/component/templates/room/roomId/photoUpload.tsx
@@ -1,32 +1,73 @@
 import { Button } from "@mantine/core";
+import { useRouter } from "next/router";
 import type { VFC } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useRef } from "react";
 
 import { Title } from "@/component/atoms/Title";
 import { supabase } from "@/utils/supabase";
 
+const removeBucketPath = (key: string, bucketName: string) => {
+  return key.slice(bucketName.length + 1);
+};
+
+const handleChange = async (
+  event: React.ChangeEvent<HTMLInputElement>,
+  roomId: string,
+  userId: string
+) => {
+  if (event.target.files === null || event.target.files.length === 0) {
+    return;
+  }
+
+  const file = event.target.files[0];
+  const fileExt = file.name.split(".").pop();
+  const fileName = `${Math.random()}.${fileExt}`;
+  const filePath = `${fileName}`;
+  const { data: inputData } = await supabase.storage
+    .from("photos")
+    .upload(filePath, file, {
+      cacheControl: "3600",
+      upsert: false,
+    });
+
+  const key = inputData?.Key;
+
+  if (!key) {
+    throw new Error("Error");
+  }
+
+  const { publicURL } = supabase.storage
+    .from("photos")
+    .getPublicUrl(removeBucketPath(key, "photos"));
+
+  await supabase
+    .from("standard_game_user_progresses")
+    .update([
+      {
+        photo_key: key,
+        photo_url: publicURL,
+      },
+    ])
+    .match({ room_id: roomId, user_id: userId });
+};
+
 export const PhotoUpload: VFC = () => {
   const inputRef = useRef(null);
+  const [userId, setUserId] = useState("");
+  const [roomId, setRoomId] = useState("");
+  const router = useRouter();
 
-  const test = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files === null || event.target.files.length === 0) {
-      return;
+  useEffect(() => {
+    if (router.asPath !== router.route) {
+      const roomId_tmp = router.asPath.split("/")[2];
+      const roomIdPath = roomId_tmp.split("?uid=")[0];
+      const userIdPath = roomId_tmp.split("?uid=")[1];
+      setUserId(userIdPath);
+      setRoomId(roomIdPath);
     }
-
-    const file = event.target.files[0];
-    const fileExt = file.name.split(".").pop();
-    const fileName = `${Math.random()}.${fileExt}`;
-    const filePath = `${fileName}`;
-    const { data } = await supabase.storage
-      .from("photos")
-      .upload(filePath, file, {
-        cacheControl: "3600",
-        upsert: false,
-      });
-
-    // eslint-disable-next-line no-console
-    console.log(data?.Key);
-  };
+  }, [router.asPath]);
 
   const fileUpload = () => {
     inputRef.current && inputRef.current.click();
@@ -47,7 +88,9 @@ export const PhotoUpload: VFC = () => {
           ref={inputRef}
           id="single"
           accept="image/*"
-          onChange={test}
+          onChange={() => {
+            handleChange(event, roomId, userId);
+          }}
         />
       </div>
     </div>
