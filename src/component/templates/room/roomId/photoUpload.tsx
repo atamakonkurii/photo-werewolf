4cee48fb8b0496fddcabcb16cefd2ae90e92715a
atamakonkurii <morisudesu@gmail.com>
2022-04-24 14:29:37 +0900

    画像アップロードボタンを装飾

--- src/component/templates/room/roomId/photoUpload.tsx
+++ src/component/templates/room/roomId/photoUpload.tsx
@@ -1,12 +1,55 @@
+import { Button } from "@mantine/core";
 import type { VFC } from "react";
+import { useRef } from "react";
 
 import { Title } from "@/component/atoms/Title";
+import { supabase } from "@/utils/supabase";
 
 export const PhotoUpload: VFC = () => {
+  const inputRef = useRef(null);
+
+  const test = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files === null || event.target.files.length === 0) {
+      return;
+    }
+
+    const file = event.target.files[0];
+    const fileExt = file.name.split(".").pop();
+    const fileName = `${Math.random()}.${fileExt}`;
+    const filePath = `${fileName}`;
+    const { data } = await supabase.storage
+      .from("photos")
+      .upload(filePath, file, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+
+    // eslint-disable-next-line no-console
+    console.log(data?.Key);
+  };
+
+  const fileUpload = () => {
+    inputRef.current && inputRef.current.click();
+  };
+
   return (
     <div className="flex flex-col justify-center items-center p-16">
       <Title title="①話し合ってテーマを決める。" />
       <Title title="②スマホのカメラロールから、テーマに沿った画像を一枚選ぶ。" />
+      <div>
+        <Button onClick={fileUpload}>画像を選択する</Button>
+        <input
+          style={{
+            visibility: "hidden",
+            position: "absolute",
+          }}
+          type="file"
+          ref={inputRef}
+          id="single"
+          accept="image/*"
+          onChange={test}
+        />
+      </div>
     </div>
   );
 };
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -2,3 +2,29 @@
 insert into auth.users (id)
 values 
   ('0d5a27ca-6263-418a-9732-5a33b19ecaf9');
+
+-- バケットとポリシーを作成
+insert into storage.buckets (id, name)
+values ('photos', 'photos');
+
+create policy "Photos are publicly accessible." on storage.objects
+  for select using (bucket_id = 'photos');
+
+create policy "Anyone can upload an photo." on storage.objects
+  for insert with check (bucket_id = 'photos');
+
+create policy "Anyone can update an photo." on storage.objects
+  for update with check (bucket_id = 'photos');
+
+-- リアルタイム通信を許可
+begin;
+  drop publication if exists supabase_realtime;
+  create publication supabase_realtime;
+commit;
+alter publication supabase_realtime
+  add table rooms;
+alter publication supabase_realtime
+  add table game_results;
+alter publication supabase_realtime
+  add table standard_game_user_progresses;
+
