0b543d8e78f89fe1e8593a802c6d55c775853b9b
atamakonkurii <morisudesu@gmail.com>
2022-04-26 08:14:32 +0900

    自分の画像を追加

--- src/component/molecules/Button/Guest.tsx
+++ src/component/molecules/Button/Guest.tsx
@@ -9,10 +9,7 @@ export const Guest = (props: Props) => {
     <div className="flex flex-col justify-between items-center p-4 hover:bg-[#f6f8f9]  duration-300 cursor-pointer sm:flex-row sm:py-4 sm:px-8">
       <div className="flex flex-col items-center text-center sm:flex-row sm:text-left">
         <div className="mb-2.5 sm:mr-2.5 sm:mb-0">
-          <img
-            className="w-20 h-20 rounded-full"
-            src="https://randomuser.me/api/portraits/men/63.jpg"
-          />
+          <img className="w-20 h-20 rounded-full" src="/images/my_image.JPG" />
         </div>
         <div className="flex flex-col mb-4 sm:mr-4 sm:mb-0">
           <a href="/" className="font-medium no-underline">
