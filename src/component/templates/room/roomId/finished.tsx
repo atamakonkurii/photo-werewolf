07006b4c2e194a5c0161e5ab6630f0849da555e2
atamakonkurii <morisudesu@gmail.com>
2022-04-26 08:26:31 +0900

    工事中

--- src/component/templates/room/roomId/finished.tsx
+++ src/component/templates/room/roomId/finished.tsx
@@ -4,8 +4,10 @@ import { Title } from "@/component/atoms/Title";
 
 export const Finished: VFC = () => {
   return (
-    <div className="flex flex-col justify-center items-center p-16">
-      <Title title="マンパワーで集計中" />
+    <div className="flex flex-col justify-center items-center p-4">
+      <Title title="工事中" />
+      <Title title="集計機能はできていません" />
+      <img src="/images/ojigi_animal_inu.png" />
     </div>
   );
 };
